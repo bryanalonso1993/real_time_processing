99b74a4abe06f07cf2caacabda045a69a3bb6c86
Bryan <52892123+bryanalonso1993@users.noreply.github.com>
2024-07-06 22:18:52 +0000

    ceacion de DAG e instalar dependencias airflow

--- airflow_dag/poblar_tablas_mysql.py
+++ airflow_dag/poblar_tablas_mysql.py
@@ -1,12 +1,16 @@
 #!/usr/env bin python
 from airflow.utils.dates import days_ago
-from datetime import datetime
 from airflow import DAG
 
-# Operador MySQL para crear los esquemas
-from airflow.operators.mysql_operator import MySqlOperator
+from airflow.hooks.base_hook import BaseHook
 from airflow.operators.python import PythonOperator
+
+from sqlalchemy import create_engine
+from datetime import datetime
 import pandas as pd
+import numpy as np
+import uuid
+
 
 DATAFILE_PEDIDOS = "/opt/airflow/repository/DATA_PEDIDOS.xlsx"
 
@@ -14,41 +18,60 @@ default_args = {
     'owner': 'Bryan Alonso'
 }
 
-def read_xlsx_file():
+def leer_xlsx():
     df = pd.read_excel(DATAFILE_PEDIDOS, header=0, date_format=True)
-    return df
+    return df.to_json()
 
-def data_wrangling(ti):
-    df = ti.xcom_pull(task_ids="read_xlsx_file")
+def limpiar_dataframe(ti):
+    json_data = ti.xcom_pull(task_ids='leer_xlsx')
+    df = pd.read_json(json_data)
     df = df.drop(['TELE', 'DIR', 'TIPVTA', 'FECFACTURA'], axis=1)
     df = df.drop(df[(df.EDAD > 100)].index)
-    return df
+    return df.to_json()
 
-def print_df(ti):
-    df = ti.xcom_pull(task_ids="data_wrangling")
-    print(df.head())
+def crear_df_pedido(ti):
+    json_data = ti.xcom_pull(task_ids='limpiar_dataframe')
+    df = pd.read_json(json_data)
+    df_pedido = df[['ESTADO', 'REGION', 'CATE', 'CANT','DSCTO']]
+    df_pedido['uuid'] = np.array([str(uuid.uuid4()) for _ in range(len(df_pedido.index))])
+    df_pedido = df_pedido.rename(columns={ 'ESTADO':'estado', 'REGION': 'region', 'CATE': 'categoria', 'uuid': 'uuid_pedido', 'CANT': 'cantidad', 'DSCTO': 'descuento' })
+    print(df_pedido.head())
+    return df_pedido.to_json()
+
+def insertar_df_pedido_mysql(ti):
+    json_data = ti.xcom_pull(task_ids='crear_df_pedido')
+    df = pd.read_json(json_data)
+    mysql_conn = BaseHook.get_connection('mysql_local_connector')
+    cnx_string = f'mysql+pymysql://{mysql_conn.login}:{mysql_conn.password}@{mysql_conn.host}:{mysql_conn.port}/{mysql_conn.schema}'
+    engine = create_engine(cnx_string)
+    df.to_sql(name='pedido', con=engine, if_exists='append', index=False)
 
 with DAG(
-    dag_id="poblar_base_datos",
-    description="pipeline para poblar la base de datos",
-    default_args=default_args,
-    start_date=days_ago(1),
-    schedule_interval="@once",
+    dag_id = "poblar_base_datos",
+    description = "pipeline para poblar la base de datos",
+    default_args = default_args,
+    start_date = days_ago(1),
+    schedule_interval = "@once",
     tags=['pipeline', 'mysql', 'dataset', 'sistema_ventas']
 ) as dag:
     task_A = PythonOperator(
-        task_id="read_xlsx_file",
-        python_callable=read_xlsx_file
+        task_id="leer_xlsx",
+        python_callable=leer_xlsx
     )
 
     task_B = PythonOperator(
-        task_id="clean_dataframe",
-        python_callable=data_wrangling
+        task_id="limpiar_dataframe",
+        python_callable=limpiar_dataframe
     )
 
     task_C = PythonOperator(
-        task_id="print_df",
-        python_callable=print_df
+        task_id="crear_df_pedido",
+        python_callable=crear_df_pedido
     )
 
-task_A >> task_B >> task_C
+    task_D = PythonOperator(
+        task_id="insertar_df_pedido_mysql",
+        python_callable=insertar_df_pedido_mysql
+    )
+
+task_A >> task_B >> task_C >> task_D
