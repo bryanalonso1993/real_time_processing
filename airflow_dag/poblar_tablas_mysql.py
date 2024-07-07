caefab32c01c42083f1ed2922641e12127c6bd8c
Bryan <52892123+bryanalonso1993@users.noreply.github.com>
2024-07-07 14:31:04 +0000

    modificando pipeline para la ingesta de multiples tablas

--- airflow_dag/poblar_tablas_mysql.py
+++ airflow_dag/poblar_tablas_mysql.py
@@ -2,8 +2,9 @@
 from airflow.utils.dates import days_ago
 from airflow import DAG
 
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.hooks.base_hook import BaseHook
-from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 
 from sqlalchemy import create_engine
 from datetime import datetime
@@ -35,7 +36,6 @@ def crear_df_pedido(ti):
     df_pedido = df[['ESTADO', 'REGION', 'CATE', 'CANT','DSCTO']]
     df_pedido['uuid'] = np.array([str(uuid.uuid4()) for _ in range(len(df_pedido.index))])
     df_pedido = df_pedido.rename(columns={ 'ESTADO':'estado', 'REGION': 'region', 'CATE': 'categoria', 'uuid': 'uuid_pedido', 'CANT': 'cantidad', 'DSCTO': 'descuento' })
-    print(df_pedido.head())
     return df_pedido.to_json()
 
 def insertar_df_pedido_mysql(ti):
@@ -45,6 +45,30 @@ def insertar_df_pedido_mysql(ti):
     cnx_string = f'mysql+pymysql://{mysql_conn.login}:{mysql_conn.password}@{mysql_conn.host}:{mysql_conn.port}/{mysql_conn.schema}'
     engine = create_engine(cnx_string)
     df.to_sql(name='pedido', con=engine, if_exists='append', index=False)
+
+def crear_df_producto(ti):
+    json_data = ti.xcom_pull(task_ids='leer_xlsx')
+    df = pd.read_json(json_data)
+    df_producto = df[['NOMPRO', 'PRECIO', 'IGV']]
+    df_producto['uuid'] = np.array([str(uuid.uuid4()) for _ in range(len(df_producto.index))])
+    df_producto = df_producto.rename(columns={ 'NOMPRO': 'nombre_producto', 'PRECIO': 'precio', 'IGV':'igv', 'uuid': 'uuid_producto' })
+    return df_producto.to_json()
+
+def insertar_df_producto_mysql(ti):
+    json_data = ti.xcom_pull(task_ids='crear_df_producto')
+    df = pd.read_json(json_data)
+    mysql_conn = BaseHook.get_connection('mysql_local_connector')
+    cnx_string = f'mysql+pymysql://{mysql_conn.login}:{mysql_conn.password}@{mysql_conn.host}:{mysql_conn.port}/{mysql_conn.schema}'
+    engine = create_engine(cnx_string)
+    df.to_sql(name='producto', con=engine, if_exists='append', index=False)
+
+def determinar_flujo_a_cargar():
+    transform_action = Variable.get("tabla_elegida", default_var=None)
+    print(f" el flujo escogido es el siguiente: { transform_action }")
+    if transform_action == 'pedido':
+        return "crear_df_pedido"
+    else:
+        return "crear_df_producto"
 
 with DAG(
     dag_id = "poblar_base_datos",
@@ -63,6 +87,11 @@ with DAG(
         task_id="limpiar_dataframe",
         python_callable=limpiar_dataframe
     )
+    
+    task_filter = BranchPythonOperator(
+        task_id="determinar_flujo_a_cargar",
+        python_callable=determinar_flujo_a_cargar
+    )
 
     task_C = PythonOperator(
         task_id="crear_df_pedido",
@@ -70,8 +99,22 @@ with DAG(
     )
 
     task_D = PythonOperator(
+        task_id="crear_df_producto",
+        python_callable=crear_df_producto
+    )
+
+    task_E = PythonOperator(
         task_id="insertar_df_pedido_mysql",
         python_callable=insertar_df_pedido_mysql
     )
 
-task_A >> task_B >> task_C >> task_D
+    task_F = PythonOperator(
+        task_id="insertar_df_producto_mysql",
+        python_callable=insertar_df_producto_mysql
+    )
+
+task_A >> task_B >> task_filter >> [task_C, task_D]
+
+task_C >> task_E
+
+task_D >> task_F
